014250cbe667d26f0fd66ff11b961fe14794f7ac
Steven Hartland <steven.hartland@multiplay.co.uk>
2022-11-12 20:25:47 +0000

    chore: make quoted failure messages more readable
    Use backtick quotes method in place of %q where possible so that
    output is more readable for simple strings with quotes.
    
    This turns:
      expected: "my thing with \"quoted parts\"`
    into:
      expected: `my thing with "quoted parts"`

--- assert/assertions.go
+++ assert/assertions.go
@@ -13,6 +13,7 @@ import (
 	"regexp"
 	"runtime"
 	"runtime/debug"
+	"strconv"
 	"strings"
 	"time"
 	"unicode"
@@ -365,6 +366,14 @@ func validateEqualArgs(expected, actual interface{}) error {
 		return errors.New("cannot take func type as argument")
 	}
 	return nil
+}
+
+// quote quotes s with backticks if possible otherwise escaped.
+func quote(s string) string {
+	if strconv.CanBackquote(s) {
+		return "`" + s + "`"
+	}
+	return strconv.Quote(s)
 }
 
 // Same asserts that two pointers reference the same object.
@@ -1429,8 +1438,8 @@ func EqualError(t TestingT, theError error, errString string, msgAndArgs ...inte
 	// don't need to use deep equals here, we know they are both strings
 	if expected != actual {
 		return Fail(t, fmt.Sprintf("Error message not equal:\n"+
-			"expected: %q\n"+
-			"actual  : %q", expected, actual), msgAndArgs...)
+			"expected: %s\n"+
+			"actual  : %s", quote(expected), quote(actual)), msgAndArgs...)
 	}
 	return true
 }
@@ -1807,8 +1816,8 @@ func ErrorIs(t TestingT, err, target error, msgAndArgs ...interface{}) bool {
 	chain := buildErrorChainString(err)
 
 	return Fail(t, fmt.Sprintf("Target error should be in err chain:\n"+
-		"expected: %q\n"+
-		"in chain: %s", expectedText, chain,
+		"expected: %s\n"+
+		"in chain: %s", quote(expectedText), chain,
 	), msgAndArgs...)
 }
 
@@ -1830,8 +1839,8 @@ func NotErrorIs(t TestingT, err, target error, msgAndArgs ...interface{}) bool {
 	chain := buildErrorChainString(err)
 
 	return Fail(t, fmt.Sprintf("Target error should not be in err chain:\n"+
-		"found: %q\n"+
-		"in chain: %s", expectedText, chain,
+		"found: %s\n"+
+		"in chain: %s", quote(expectedText), chain,
 	), msgAndArgs...)
 }
 
@@ -1859,9 +1868,9 @@ func buildErrorChainString(err error) string {
 	}
 
 	e := errors.Unwrap(err)
-	chain := fmt.Sprintf("%q", err.Error())
+	chain := quote(err.Error())
 	for e != nil {
-		chain += fmt.Sprintf("\n\t%q", e.Error())
+		chain += fmt.Sprintf("\n\t%s", quote(e.Error()))
 		e = errors.Unwrap(e)
 	}
 	return chain
